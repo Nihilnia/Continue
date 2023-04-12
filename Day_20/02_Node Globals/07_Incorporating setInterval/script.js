a38152f2e3b09731dd8b9906edcebd3109bb2895
Nihil <35506793+Nihilnia@users.noreply.github.com>
2023-04-12 16:57:27 +0300

    setTimeout, setInterval nd clearInterval bra

--- Day_20/02_Node Globals/07_Incorporating setInterval/script.js
+++ Day_20/02_Node Globals/07_Incorporating setInterval/script.js
@@ -1,11 +1,32 @@
+const grab = (f) => {
+  let index = process.argv.indexOf(f) + 1;
+  return process.argv[index];
+};
+
+let delay = grab("--delay");
 let key = 0;
-const second = setInterval(() => {
+
+const timerFinished = () => {
+  clearInterval(interval);
+  console.log("Timer is done. Been: " + delay + "seconds");
+};
+
+setTimeout(timerFinished, delay * 1000);
+
+const myInterval = () => {
   key += 0.5;
-  console.log(`${key} second passed..`);
-}, 500);
+  console.log(`${key} seconds passed..`);
+};
 
-let stop = clearInterval(second);
+const interval = setInterval(myInterval, 500);
 
-if (key >= 10) {
-  stop();
-}
+// or, shorter
+
+// let key = 0;
+// const xInterval = setInterval(() => {
+//   key += 0.5;
+//   console.log(`${key} seconds passed..`);
+//   if (key === 5) {
+//     clearInterval(xInterval);
+//   }
+// }, 500);
